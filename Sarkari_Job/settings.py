5450588a745ad4735fd819f0d71a22aca9c192ac
Deepak5011 <mydribblecoin@gmail.com>
2025-04-06 11:19:19 +0530

    Update settings.py

--- Sarkari_Job/settings.py
+++ Sarkari_Job/settings.py
@@ -75,13 +75,22 @@ WSGI_APPLICATION = 'Sarkari_Job.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'database',
+        'USER': 'admin',
+        'PASSWORD': 'Depak29896#',
+        'HOST': 'database.cn80i6y407rl.eu-north-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
